990e87eca0b9fa949695d224015eebc6fa54d270
Gnoud98 <hiepsilun98@gmail.com>
2020-06-13 11:25:46 +0700

    season1

---
+++ bt1.js
@@ -0,0 +1,35 @@
+// #1
+let message = "Coding is great";
+console.log(message)
+    //2
+let studentcount = 0;
+console.log(studentcount);
+//3
+message = "Coding might not easy,but still great";
+console.log(message);
+//4
+studentcount = 20;
+console.log(studentcount);
+let a = message.toLowerCase()
+console.log(a);
+let b = studentcount;
+b++;
+console.log(b);
+let c = "Hôm nay bạn thật tuyệt"
+alert(c);
+let ask = prompt("Bạn tên là gì?")
+alert("Xin chào " + ask);
+//
+let name = prompt("Bạn họ gì ?");
+let name2 = prompt("Tên bạn là gì?")
+alert("Xin chào " +
+    name + name2);
+let dt = prompt("Hãy nhập cạnh của hình vuông");
+dientich = dt * dt;
+alert("Diện tích hình vuông là : " + dientich);
+let cicle = prompt("Nhập bán kính hình tròn");
+bankinh = cicle * cicle;
+alert("Diện tích hình tròn là :" + bankinh * (Math.PI));
+let doC = prompt("Nhập vào độ C :")
+doF = (9 / 5) * doC + 32;
+alert(doF);
