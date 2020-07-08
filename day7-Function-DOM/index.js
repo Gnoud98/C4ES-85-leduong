46dfaee62216215ec26c7e1362b8f7235119143a
Lê Quang Dương <66775619+Gnoud98@users.noreply.github.com>
2020-07-08 16:10:09 +0700

    hw

---
+++ day7-Function-DOM/index.js
@@ -0,0 +1,34 @@
+// sayHi('nam', 'function is great');
+
+// function sayHi(name, comment) {
+//     alert('hi' + " " + name + " " + comment);
+// }
+
+// function tinhTong(a, b) {
+//     return (a + b)
+// }
+// tinhTong(1, 5);
+// alert(tinhTong(1, 5))
+// function conTho() {
+//     setTimeout(function() {
+
+//         console.log("con tho");
+//     }, 3000);
+// }
+//callback
+// function conTho(callback007) {
+//     setTimeout(() => {
+//         console.log("con tho")
+//         callback007();
+//     }, 3000);
+// }
+
+// function hoTel() {
+//     console.log("an co")
+// }
+// var callback = function() {
+//     hoTel();
+// }
+// conTho(callback);
+//declaration
+//expression
